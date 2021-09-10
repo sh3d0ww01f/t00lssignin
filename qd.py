633e7c033256c5bec22effadc4410d2d4b6f89e6
sh3d0ww01f <31366374+sh3d0ww01f@users.noreply.github.com>
2021-09-10 17:26:10 +0800

    Create qd.py

--- qd.py
+++ qd.py
@@ -52,7 +52,7 @@ def t00ls_login(u_name, u_pass, q_num, q_ans):
         'questionid': q_num,
         'answer': q_ans
     }
-    response_login = requests.post('https://www.t00ls.net/login.json', data=login_data, headers=req_headers)
+    response_login = requests.post('https://www.t00ls.cc/login.json', data=login_data, headers=req_headers)
     response_login_json = json.loads(response_login.text)
 
     if response_login_json['status'] != 'success':
@@ -85,7 +85,7 @@ def t00ls_sign(t00ls_hash, t00ls_cookies):
         'formhash': t00ls_hash,
         'signsubmit': "true"
     }
-    response_sign = requests.post('https://www.t00ls.net/ajax-sign.json', data=sign_data, cookies=t00ls_cookies,headers=req_headers)
+    response_sign = requests.post('https://www.t00ls.cc/ajax-sign.json', data=sign_data, cookies=t00ls_cookies,headers=req_headers)
     return json.loads(response_sign.text)
 def getdomain():
     headers = {
@@ -114,7 +114,7 @@ def t00ls_domain(t00ls_hash, t00ls_cookies):
 
     start_time = time.time()
 
-    query_url = 'https://www.t00ls.net/domain.html'
+    query_url = 'https://www.t00ls.cc/domain.html'
     query_status = False
     query_count = 0  # 查询重试次数
 
@@ -132,7 +132,7 @@ def t00ls_domain(t00ls_hash, t00ls_cookies):
         except Exception:
             pass
         if domain in response_query.text:
-            response_tb = requests.get('https://www.t00ls.net/members-tubilog.json', cookies=t00ls_cookies)
+            response_tb = requests.get('https://www.t00ls.cc/members-tubilog.json', cookies=t00ls_cookies)
             if domain in response_tb.text:
                 print('查询域名成功 TuBi + 1 \n')
                 content += '查询域名成功 TuBi + 1\n'
